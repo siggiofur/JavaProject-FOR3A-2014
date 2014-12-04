90abc70154d18a321026dbe53f8878a65c4aba94
Steinar Snær Guðjónsson <steinar.snaer.gudjonsson@gmail.com>
2014-12-04 16:32:15 +0000

    nothing important
    just clean up

--- webServer/README.md
+++
@@ -1,4 +0,0 @@
-JavaProject-FOR3A-2014
-======================
-Project is being worked on by Me, Theep and Neverstone,
-BattleChat game using streams, sockets, threads and other such useful things.
--- webServer/server.js
+++ webServer/server.js
@@ -1,8 +1,9 @@
-var app 		= require('express')();
-var express		= require('express');
-var http 		= require('http').Server(app);
-var io 			= require('socket.io')(http);
+var app         = require('express')();
+var express     = require('express');
+var http        = require('http').Server(app);
+var io          = require('socket.io')(http);
 var bodyParser  = require('body-parser');    // importing body-parser
+var net         = require('net');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,31 +19,49 @@ app.use(express.static('public'));
 
 var router = express.Router(); // get an instance of the express Router
 
+
+var net = require('net');
+
+var javalink = createServer(function (socket) {
+    console.log("connected");
+
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+})
+
+javalink.listen(4445, function() { //'listening' listener
+    console.log('javalink bound');
+});
+
+
+
 //routes
 router.get('/', function(req, res) {
-	res.render('index'); 
+    res.render('index'); 
 });
 
 // all of our routes will be prefixed with /api
 app.use('/', router);
 
 io.on('connection', function(socket){
-	console.log('user connected');
+    console.log('user connected');
 
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-		console.log('message: ' + msg);
-	});
+    socket.on('chat message', function(msg){
+        javalink.write(socket.id + ':' + msg);
+        console.log(socket.id + 'message: ' + msg);
+    });
 
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
 http.listen(app.get('port'), function(){
-	console.log('listening on *:' + app.get('port'));
+    console.log('listening on *:' + app.get('port'));
 });
 
 // Server start
 //app.listen(app.get('port'), '0.0.0.0');
 //console.log('Magic happens on port ' + app.get('port'));
+
